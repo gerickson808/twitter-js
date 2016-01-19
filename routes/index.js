1d0b292c26a3860492608b49e4db50feb8006c34
Gustaf Erickson <gerickson808@gmail.com>
2016-01-19 18:08:56 -0500

    Minor bug fixes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 var router = require('express').Router();
 // could use one line instead: var router = require('express').Router();
 var tweetBank = require('../tweetBank');
+var bodyParser = require('body-parser');
+
+bodyParser.json();
+bodyParser.urlencoded();
 
 router.get('/', function (req, res) {
   var tweets = tweetBank.list();
-  res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
+	res.render( 'index', { title: 'Twitter.js', tweets: tweets, showForm: true } );
 });
 
 // say that a client GET requests the path /users/nimit
@@ -19,6 +23,13 @@ router.get('/tweets/:id', function(req, res) {
   var id = Number(req.params.id);
   var list = tweetBank.find( {id: id} );
   res.render( 'index', { title: 'Twitter.js - Post #'+id, tweets: list } );
+});
+
+router.post('/tweets', function(req, res) {
+  var name = req.body.name;
+  var text = req.body.text;
+  tweetBank.add(name, text);
+  res.redirect('/');
 });
 
 module.exports = router;
