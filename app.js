29bc9639e4df9768c5a65ddc4c3c2c8d85bc978f
Gustaf Erickson <gerickson808@gmail.com>
2016-01-19 17:04:40 -0500

    tweeter

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var swig = require('swig');
-var routes = require('routes');
+var routes = require('./routes');
 
 var output;
 var people = {
@@ -18,9 +18,9 @@ app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 app.set('views', './views');
 
+app.use(express.static('public'));
 
 app.use('/', routes);
-
 
 
 
