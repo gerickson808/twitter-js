d50505e1c54e9f160208b91f482acf54f86853e9
khumphrey <khumphrey7890@gmail.com>
2016-01-19 16:52:28 -0500

    all the goodz

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var swig = require('swig');
+var routes = require('routes');
+
 var output;
 var people = {
 	1:{name: 'Gandalf'},
@@ -17,37 +19,59 @@ app.set('view engine', 'html');
 app.set('views', './views');
 
 
-app.get('/swiggums', function(request, response){
-	response.render('index', locals);
-});
-
-app.use(function(response, request, next){
-	output = "";
-	next();
-});
-
-app.use('/special', function(request, response, next){
-	output += "Hey, special person.\n";
-	next();
-});
-
-app.get('/', function(request, response, next){
-	output += "Yooooooooo\n";
-	next();
-});
+app.use('/', routes);
 
 
-app.get('/news', function(request, response, next){
-	output += "Hey, there's some Bird News!\n";
-	next();
-});
-
-app.use(function(request, response){
-	//output += request.method+" "+request.path+" "+response.statusCode;
-	response.send(output);
-});
 
 
 app.listen(3000, function(){
 	console.log('server listening');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.get('/swiggums', function(request, response){
+// 	response.render('index', locals);
+// });
+
+// app.use(function(response, request, next){
+// 	output = "";
+// 	next();
+// });
+
+// app.use('/special', function(request, response, next){
+// 	output += "Hey, special person.\n";
+// 	next();
+// });
+
+// app.get('/', function(request, response, next){
+// 	output += "Yooooooooo\n";
+// 	next();
+// });
+
+
+// app.get('/news', function(request, response, next){
+// 	output += "Hey, there's some Bird News!\n";
+// 	next();
+// });
+
+// app.use(function(request, response){
+// 	//output += request.method+" "+request.path+" "+response.statusCode;
+// 	response.send(output);
+// });
