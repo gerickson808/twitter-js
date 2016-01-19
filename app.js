24610f8abb30c64a47f2ac5e80d54221b9a5bb3c
Gustaf Erickson <gerickson808@gmail.com>
2016-01-19 15:14:14 -0500

    Fake Middleware

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var app = express();
+var output;
 
-app.get('/', function(request, response){
-	response.send("Yooooooooo");
+app.use(function(response, request, next){
+	output = "";
+	next();
 });
+app.use('/special', function(request, response, next){
+	output += "Hey, special person.\n";
+	next();
+});
+
+app.get('/', function(request, response, next){
+	output += "Yooooooooo\n";
+	next();
+});
+
+app.get('/news', function(request, response, next){
+	output += "Hey, there's some Bird News!\n";
+	next();
+});
+
+app.use(function(request, response){
+	output += request.method+" "+request.path+" "+response.statusCode;
+	response.send(output);
+});
+
 
 app.listen(3000, function(){
 	console.log('server listening');
