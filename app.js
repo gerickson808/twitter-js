0a3aeed20c67c5147e09bafabed3f3f6aabdb4a2
Gustaf Erickson <gerickson808@gmail.com>
2016-01-19 15:40:54 -0500

    Template Madness

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
+var swig = require('swig');
 var express = require('express');
 var app = express();
 var output;
+var people = {
+	1:{name: 'Gandalf'},
+	2:{name: 'Frodo'},
+	3:{name: 'Hermione'}
+};
+
+var title = "Template Madness Y'all";
+var locals = {title:title, people:people};
 
 app.use(function(response, request, next){
 	output = "";
 	next();
 });
+
 app.use('/special', function(request, response, next){
 	output += "Hey, special person.\n";
 	next();
@@ -16,13 +26,18 @@ app.get('/', function(request, response, next){
 	next();
 });
 
+app.get('/swiggums', function(request, response, next){
+	output = swig.renderFile('./views/index.html',locals);
+	next();
+});
+
 app.get('/news', function(request, response, next){
 	output += "Hey, there's some Bird News!\n";
 	next();
 });
 
 app.use(function(request, response){
-	output += request.method+" "+request.path+" "+response.statusCode;
+	//output += request.method+" "+request.path+" "+response.statusCode;
 	response.send(output);
 });
 
